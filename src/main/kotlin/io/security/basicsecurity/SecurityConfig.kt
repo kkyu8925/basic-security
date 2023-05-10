56dc52d4ed4af03911b7f841587d16d3c9342a6a
kkyu8925 <kkyu8925@gmail.com>
2023-05-10 22:46:32 +0900

    10) 세션 제어 필터 : SessionManagementFilter, ConcurrentSessionFilter

--- src/main/kotlin/io/security/basicsecurity/SecurityConfig.kt
+++ src/main/kotlin/io/security/basicsecurity/SecurityConfig.kt
@@ -20,40 +20,10 @@ class SecurityConfig(
                 it.anyRequest().authenticated()
             }
             .formLogin {
-                it.defaultSuccessUrl("/")
-                    .failureUrl("/login")
-                    .usernameParameter("userId")
-                    .passwordParameter("passwd")
-                    .loginProcessingUrl("/login_proc")
-                    .successHandler { request, response, authentication ->
-                        println("authentication : ${authentication.name}")
-                        response.sendRedirect("/")
-                    }
-                    .failureHandler { request, response, exception ->
-                        println("exception: ${exception.message}")
-                        response.sendRedirect("/login")
-                    }
-                    .permitAll()
-            }
-            .logout {
-                it.logoutUrl("/logout")
-                    .logoutSuccessUrl("/login")
-                    .deleteCookies("JSESSIONID", "remember-me")
-                    .addLogoutHandler { request, response, authentication ->
-                        val session = request.session
-                        println("session: $session")
-                        session.invalidate()
-                    }.logoutSuccessHandler { request, response, authentication ->
-                        response.sendRedirect("/login")
-                    }
-            }
-            .rememberMe {
-                it.rememberMeParameter("remember-me")
-                    .tokenValiditySeconds(3600)
-                    .userDetailsService(userDetailsService)
             }
             .sessionManagement {
                 it.maximumSessions(1)
+                    .maxSessionsPreventsLogin(true)
             }
             .build()
     }
