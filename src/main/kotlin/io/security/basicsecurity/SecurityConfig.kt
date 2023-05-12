78b78665c83432626f8a0cdc9dac5f11d9c4d1a5
kkyu8925 <kkyu8925@gmail.com>
2023-05-12 23:31:49 +0900

    13) 사이트 간 요청 위조 - CSRF, CsrfFilter

--- src/main/kotlin/io/security/basicsecurity/SecurityConfig.kt
+++ src/main/kotlin/io/security/basicsecurity/SecurityConfig.kt
@@ -4,69 +4,20 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
 
     @Bean
-    fun userDetailsService(): UserDetailsService {
-        val user = User
-            .withUsername("user")
-            .password("{noop}1234")
-            .roles("USER")
-            .build()
-
-        val sys = User
-            .withUsername("sys")
-            .password("{noop}1234")
-            .roles("SYS")
-            .build()
-
-        val admin = User
-            .withUsername("admin")
-            .password("{noop}1234")
-            .roles("ADMIN")
-            .build()
-
-        return InMemoryUserDetailsManager(user, sys, admin)
-    }
-
-    @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         return http
             .authorizeHttpRequests {
                 it
-                    .requestMatchers("/user").hasRole("USER")
-                    .requestMatchers("/admin/sys").hasRole("SYS")
-                    .requestMatchers("/admin/**").hasAnyRole("SYS", "ADMIN")
                     .anyRequest().authenticated()
             }
             .formLogin {
-                it.successHandler { request, response, authentication ->
-                    val httpSessionRequestCache = HttpSessionRequestCache()
-                    val savedRequest = httpSessionRequestCache.getRequest(request, response)
-                    response.sendRedirect(savedRequest.redirectUrl)
-                }
-            }
-            .sessionManagement {
-                it.maximumSessions(1)
-//                    .maxSessionsPreventsLogin(true)
-                    .maxSessionsPreventsLogin(false)
-            }
-            .exceptionHandling {
-                it
-                    .authenticationEntryPoint { request, response, authException ->
-                        response.sendRedirect("/login")
-                    }
-                    .accessDeniedHandler { request, response, accessDeniedException ->
-                        response.sendRedirect("/denied")
-                    }
             }
             .build()
     }
